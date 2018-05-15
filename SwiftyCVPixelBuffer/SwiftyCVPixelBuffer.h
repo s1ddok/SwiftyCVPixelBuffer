085754748c3b53555432f59fb813433a0bca7dac
s1ddok <siddok@gmail.com>
2018-05-15 21:37:40 +0300

    Initial Commit

---
+++ SwiftyCVPixelBuffer/SwiftyCVPixelBuffer.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyCVPixelBuffer.h
+//  SwiftyCVPixelBuffer
+//
+//  Created by Andrey Volodin on 15.05.2018.
+//  Copyright © 2018 Andrey Volodin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyCVPixelBuffer.
+FOUNDATION_EXPORT double SwiftyCVPixelBufferVersionNumber;
+
+//! Project version string for SwiftyCVPixelBuffer.
+FOUNDATION_EXPORT const unsigned char SwiftyCVPixelBufferVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCVPixelBuffer/PublicHeader.h>
+
+
